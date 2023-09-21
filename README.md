c0e0421603b66e3a643ad87a6ff7a9ec42757624
Manohar Reddy <b.manu199@gmail.com>
2023-09-21 22:48:30 +0200

    update README.md and fix configs

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 ### introduction
 
+An application that monitors wallets on the kumama network and alerts when there is change in the wallet.
+
+wallets are defined in addresses-config configmap
+
+### usage
+
+just run:
+```
+start.sh
+```
+
+### components:
+
+First installs k8s prometheus operator
+Next installs mailhog operator so that AlertManager can send emails to this
+Final install the application. In the format of helm templates.
+
+We can see the alerts in alertmanager
+
+```
+kubectl port-forward svc/prometheus-kube-prometheus-alertmanager 9093:9093
+```
+
+### TODO:
+* need to understand why the alert is not showing up in mailhog.
+
--- start.sh
+++ start.sh
@@ -6,10 +6,24 @@ kind create cluster --name watcher
 
 # setup and install prometheus operator
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo add codecentric https://codecentric.github.io/helm-charts
+
 helm repo update
+
 helm install --wait prometheus prometheus-community/kube-prometheus-stack \
 --set kubelet.serviceMonitor.https=true \
---set prometheus.prometheusSpec.serviceMonitorSelectorNilUsesHelmValues=false
+--set prometheus.prometheusSpec.serviceMonitorSelectorNilUsesHelmValues=false \
+--set alertmanager.alertmanagerSpec.replicas=3
+
+helm install mailhog codecentric/mailhog
 
 # helm the helm chart
 helm install --wait watcher ./helm
+
+echo "wallet addresses are provided in the configmap named: addresses-config"
+
+echo "One of the wallet JFKE9s3nvNu4p6P5u5qL2xz1j5b43ScwcAD8Bja1opWDj8k has zero balance in it"
+echo "you could see the alert firing in the below email link. which open mailhog (a simple testing email client)"
+
+export POD_NAME=$(kubectl get pods --namespace default -l "app.kubernetes.io/name=mailhog,app.kubernetes.io/instance=mailhog" -o jsonpath="{.items[0].metadata.name}")
+kubectl port-forward --namespace default $POD_NAME 8025
